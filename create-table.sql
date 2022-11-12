2592dcdb3b169fde3b70353e639258d102f628fa
tguo4 <tguo4@student.gsu.edu>
2022-11-12 00:20:29 -0500

    added manager table

--- create-table.sql
+++ create-table.sql
@@ -1,12 +1,14 @@
-copy manager FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\manager.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
-copy manager-team FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\manager-team.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
-copy appearance FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\appearance.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
-copy game FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\game.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
-copy team FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\team.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
-copy gameTeam FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\gameTeam.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
-copy shot FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\shot.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
-copy venue FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\venue.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
-copy league FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\league.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
-copy season FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\season.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
-copy player FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\player.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+heroku pg:psql -a damp-eyrie-64951
+
+\copy manager FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\manager.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\copy manager-team FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\manager-team.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\copy appearance FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\appearance.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\copy game FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\game.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\copy team FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\team.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\copy gameTeam FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\gameTeam.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\copy shot FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\shot.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\copy venue FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\venue.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\copy league FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\league.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\copy season FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\season.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
+\copy player FROM C:\Users\lurgi\Documents\GitHub\database-grad-project-fall-22\data\player.csv WITH (FORMAT CSV, DELIMITER ',', HEADER true);
 
