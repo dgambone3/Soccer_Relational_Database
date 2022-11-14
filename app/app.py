cadf88ca9bea3016695d710ff24b45a307f4b304
tguo4 <tguo4@student.gsu.edu>
2022-11-14 00:32:37 -0500

    template

--- app/app.py
+++ app/app.py
@@ -1,20 +1,10 @@
-import os
+
 import psycopg2
 from flask import Flask, render_template
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-
-
-
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wpepkodmgckzze:02be78245346d6bc03fa4e5e667e3904529f762923b9ce6f3c5527cbf06c5858@ec2-34-230-153-41.compute-1.amazonaws.com:5432/d79mtf79npaf4h'
 
-
-@app.route('/route_name')
-def method_name():
-    pass
 
 @app.route('/')
 def hello():
