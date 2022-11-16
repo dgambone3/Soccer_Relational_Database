6d28aa26aa64d51e578642a3549593ce58d2d197
tguo4 <tguo4@student.gsu.edu>
2022-11-16 00:41:59 -0500

    add data success

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 
 import psycopg2
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-from app.forms import ManagerTeamForm
+from sqlalchemy import func
+from forms import ManagerTeamForm
 
 
 # create the db instance
@@ -18,7 +19,7 @@ db.init_app(app)
 class Manager(db.Model):
     __tablename__ = 'manager'
 
-    manager_id = db.Column(db.Integer, primary_key=True)
+    manager_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     manager_name = db.Column(db.VARCHAR(length=50))
     
     def save_manager(self):
@@ -28,7 +29,7 @@ class Manager(db.Model):
 class Team(db.Model):
     __tablename__ = 'team'
 
-    team_id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     venue_id = db.Column(db.Integer)
     team_name = db.Column(db.VARCHAR(length=50))
     
@@ -39,9 +40,9 @@ class Team(db.Model):
 class ManagerTeam(db.Model):
     __tablename__ = 'managerteam'
     
-    contract_id = db.Column(db.Integer, primary_key=True)
-    manager_id = db.Column('Manager', db.ForeignKey('manager.manager_id'))
-    team_id = db.Column('Team', db.ForeignKey('team.team_id'))
+    contract_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    manager_id = db.Column('manager_id', db.ForeignKey('manager.manager_id'),nullable=False)
+    team_id = db.Column('team_id', db.ForeignKey('team.team_id'),nullable=False)
     start_date = db.Column(db.DateTime, nullable=False)
     end_date = db.Column(db.DateTime, nullable=True)
     
@@ -57,19 +58,32 @@ def index():
 
 @app.route('/demo', methods=['GET','POST'])
 def demo():
-  return render_template('app/demo_queries/templates/demo.html')
+  return render_template('demo.html')
 
 @app.route('/form', methods=['GET','POST'])
 def mtform():
   mtform = ManagerTeamForm()
-  if mtform.validate_on_submit():
+  # if mtform.validate_on_submit():
+  if request.method == 'POST':
+    # last_m_id = db.session.execute(db.select(Manager.manager_id).order_by(Manager.manager_id))
+    last_m_id = db.session.execute(db.select(func.max(Manager.manager_id))).scalar()
+    new_m_id = last_m_id + 1
+    last_t_id = db.session.execute(db.select(func.max(Team.team_id))).scalar()
+    new_t_id = last_t_id + 1
+    last_mt_id = db.session.execute(db.select(func.max(ManagerTeam.contract_id))).scalar()
+    new_mt_id = last_mt_id + 1
     new_manager = Manager(
+                manager_id = new_m_id,
                 manager_name = mtform.manager_name.data
                 )
     new_team = Team(
+                team_id = new_t_id,
                 team_name = mtform.team.data 
                 )
     new_managerteam = ManagerTeam(
+                contract_id = new_mt_id,
+                manager_id = new_m_id,
+                team_id = new_t_id,
                 start_date = mtform.start_date.data,
                 end_date = mtform.end_date.data 
                 )
@@ -78,6 +92,7 @@ def mtform():
     new_manager.save_manager() 
     new_team.save_team()
     new_managerteam.save_managerteam()
+    
     
     return redirect(url_for('index'))
       
