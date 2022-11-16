b16f83a41d087e68d5a46d7ebfd44ed0bbdcb627
tguo4 <tguo4@student.gsu.edu>
2022-11-15 21:52:38 -0500

    Revert "cleanup flask app avoid circular ref"
    This reverts commit 1adbf7fdeef96c44db703b2bb276f269f28af34d.

--- app/app.py
+++ app/app.py
@@ -1,20 +1,59 @@
 
+import psycopg2
 import os
 from flask import Flask, render_template, redirect, url_for
-from app import app, db
+from flask_sqlalchemy import SQLAlchemy
 from app.forms import ManagerTeamForm
-from app.models import Manager, Team, ManagerTeam
 
+
+# create the extension
+db = SQLAlchemy()
+class Manager(db.Model):
+    __tablename__ = 'manager'
+
+    manager_id = db.Column(db.Integer, primary_key=True)
+    manager_name = db.Column(db.VARCHAR(length=50))
+    
+    def save_manager(self):
+        db.session.add(self)
+        db.session.commit()
+
+class Team(db.Model):
+    __tablename__ = 'team'
+
+    team_id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer)
+    team_name = db.Column(db.VARCHAR(length=50))
+    
+    def save_team(self):
+        db.session.add(self)
+        db.session.commit()
+
+class ManagerTeam(db.Model):
+    __tablename__ = 'managerteam'
+    
+    contract_id = db.Column(db.Integer, primary_key=True)
+    manager_id = db.Column('Manager', db.ForeignKey('manager.manager_id'))
+    team_id = db.Column('Team', db.ForeignKey('team.team_id'))
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=True)
+    
+    def save_managerteam(self):
+        db.session.add(self)
+        db.session.commit()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wpepkodmgckzze:02be78245346d6bc03fa4e5e667e3904529f762923b9ce6f3c5527cbf06c5858@ec2-34-230-153-41.compute-1.amazonaws.com:5432/d79mtf79npaf4h'
+app.secret_key = os.environ.get('SECRET_KEY', 'dev')
+db.init_app(app)
 
 @app.route('/', methods=['GET','POST'])
 def index():
   return render_template('index.html')
 
-
 @app.route('/demo', methods=['GET','POST'])
 def demo():
   return render_template('app/demo_queries/templates/demo.html')
-
 
 @app.route('/form', methods=['GET','POST'])
 def mtform():
@@ -34,7 +73,7 @@ def mtform():
     # Save game method
     new_manager.save_manager() 
     new_team.save_team()
-    new_managerteam.save_managerteam()
+    new_managerteam.save_mt()
     
     return redirect(url_for('index'))
       
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from . import db
+from app.app import db
 
 class Manager(db.Model):
     __tablename__ = 'manager'
